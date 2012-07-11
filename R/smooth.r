12ba8a787510a8a56caa6c7536a662184dabf246
zarquon42b <zarquon42@gmail.com>
2012-07-11 14:29:54 +0200

    added Rclost for closest point search using vcglib.

--- R/smooth.r
+++ R/smooth.r
@@ -1,4 +1,4 @@
-vcgSmooth <- function(mesh,type=c("taubin","laplace"),iteration=10)
+vcgSmooth <- function(mesh,type=c("taubin","laplace"),iteration=10,sign=F)
   {
     type <- type[1]
     vb <- mesh$vb[1:3,]
@@ -7,8 +7,9 @@ vcgSmooth <- function(mesh,type=c("taubin","laplace"),iteration=10)
     dimvb <- dim(vb)[2]
     method <- 0
     if (type == "laplace")
-      {method <- 1
-     }
+      {
+        method <- 1
+      }
     iter=iteration
     storage.mode(it) <- "integer"
     storage.mode(method) <- "integer"
@@ -20,5 +21,23 @@ vcgSmooth <- function(mesh,type=c("taubin","laplace"),iteration=10)
     mesh$normals <- rbind(tmp[[7]],1)
     invisible(mesh)
   }
-
+vcgClost <- function(mesh,mesh2,sign=TRUE)
+  
+  {
+    vb <- mesh2$vb[1:3,]
+    it <- mesh2$it - 1
+    dimit <- dim(it)[2]
+    dimvb <- dim(vb)[2]
+    storage.mode(it) <- "integer"
+    clost <- mesh$vb[1:3,]
+    normals <- clost
+    clostDim <- ncol(clost)
+    dis <- rep(0,clostDim)
+    sign <- as.integer(sign)
+    tmp <- .C("Rclost",vb,ncol(vb),it,ncol(it),clost,clostDim,normals,dis,sign)
+    mesh$vb[1:3,] <- tmp[[5]]
+    mesh$normals <- rbind(tmp[[7]],1)
+    mesh$quality <- tmp[[8]]
+    return(mesh)
+  }
     
--- src/Rsmooth.cpp
+++ src/Rsmooth.cpp
@@ -26,6 +26,8 @@ using namespace std;
 #include <vcg/complex/algorithms/smooth.h>
 #include<vcg/complex/allocate.h>
 #include <wrap/callback.h>
+#include <vcg/complex/append.h>
+
 // VCG File Format Importer/Exporter
 #include <wrap/io_trimesh/import.h>
 #include <wrap/io_trimesh/export.h>
@@ -33,7 +35,7 @@ using namespace std;
 #include <vcg/complex/algorithms/update/color.h>
 #include <wrap/ply/plylib.cpp>
 
-extern "C" {
+
   
   
   using namespace vcg;
@@ -52,7 +54,7 @@ extern "C" {
   typedef typename MyMesh::VertexIterator VertexIterator;
   typedef typename MyMesh::VertexPointer  VertexPointer;
   typedef typename MyMesh::FaceIterator   FaceIterator;
-  
+  extern "C" {
   void vert2file(double *data ,int *dim)
   {
     typedef typename MyMesh::CoordType CoordType;
@@ -86,8 +88,6 @@ extern "C" {
 	   (*vi).P() = CoordType(x,y,z);*/
 	++vi;
       }
-    
-    //tri::io::ExporterPLY<MyMesh>::Save(m,"tt.ply",tri::io::Mask::IOM_VERTNORMAL, false); // in ASCII
   }
 
 
@@ -128,7 +128,6 @@ extern "C" {
 	(*fi).V(2)=ivp[itz];
 	++fi;
       }
-    //printf("%i\n",iter);
     if (method == 0)
       {
 	tri::Smooth<MyMesh>::VertexCoordTaubin(m,iter,0.5,-0.53);
@@ -140,7 +139,6 @@ extern "C" {
     tri::UpdateNormals<MyMesh>::PerVertexAngleWeighted(m);
     tri::UpdateNormals<MyMesh>::NormalizeVertex(m);
    
-    
     //write back output
     vi=m.vert.begin();
     for (i=0; i<d; i++) 
@@ -153,7 +151,118 @@ extern "C" {
 	normals[i*3+2] = (*vi).N()[2];
 	++vi;
       }
+  }
+  void Rclost(double *vb ,int *dim, int *it, int *dimit, double *clost, int *clostDim, double *normals, double *dis,int *sign)
+  {
+    typedef typename MyMesh::CoordType CoordType;
+    typedef typename MyMesh::ScalarType ScalarType;
+    typedef vcg::GridStaticPtr<MyMesh::FaceType, MyMesh::ScalarType> TriMeshGrid;
+    
+    ScalarType x,y,z;
+    int i;
+    
+    MyMesh m;
+    MyMesh refmesh;
+    MyMesh outmesh;
+    // section read from input
+    const int d = *dim;
+    const int faced = *dimit;
+    const int dref = *clostDim;
+    int signo = *sign;
+   
+
+    //Allocate target
+    vcg::tri::Allocator<MyMesh>::AddVertices(m,d);
+    vcg::tri::Allocator<MyMesh>::AddFaces(m,faced);
+    VertexPointer ivp[d];
+    VertexIterator vi=m.vert.begin();
+    for (i=0; i<d; i++) 
+      {
+	ivp[i]=&*vi;
+	x = vb[i*3];
+	y = vb[i*3+1];
+	z=  vb[i*3+2];
+	(*vi).P() = CoordType(x,y,z);
+	++vi;
+      }
+    int itx,ity,itz;
+    FaceIterator fi=m.face.begin();
+    for (i=0; i < faced; i++) 
+      {
+	itx = it[i*3];
+	ity = it[i*3+1];
+	itz = it[i*3+2];
+	(*fi).V(0)=ivp[itx];
+	(*fi).V(1)=ivp[ity];
+	(*fi).V(2)=ivp[itz];
+	++fi;
+      }
+    vcg::tri::Allocator<MyMesh>::AddVertices(refmesh,dref);
+    VertexPointer ivref[dref];
+    
+    vi=refmesh.vert.begin();
+    for (i=0; i<d; i++) 
+      {
+	ivref[i]=&*vi;
+	x = clost[i*3];
+	y = clost[i*3+1];
+	z = clost[i*3+2];
+	(*vi).P() = CoordType(x,y,z);
+	++vi;
+      }
+
+    vcg::tri::Append<MyMesh,MyMesh>::Mesh(outmesh,refmesh);
+    tri::UpdateBounding<MyMesh>::Box(m);
+    tri::UpdateNormals<MyMesh>::PerVertexAngleWeighted(m);
+    tri::UpdateNormals<MyMesh>::NormalizeVertex(m);
+    float maxDist = m.bbox.Diag();
+    float minDist = 1e-10;
+    
+    vcg::tri::FaceTmark<MyMesh> mf; 
+    mf.SetMesh( &m );
+    vcg::face::PointDistanceBaseFunctor<float> PDistFunct;
+    //tri::UpdateFlags<MyMesh>::FaceProjection(mesh);
+    TriMeshGrid static_grid;    
+    static_grid.Set(m.face.begin(), m.face.end());
+    
+    
+    for(int i=0; i<refmesh.vn; i++)
+      {
+	
+	Point3f& currp = refmesh.vert[i].P();
+	Point3f& clost = outmesh.vert[i].P();
+	MyFace* f_ptr= GridClosest(static_grid, PDistFunct, mf, currp, maxDist, minDist, clost);
+	
+	int f_i = vcg::tri::Index(m, f_ptr);
+	MyMesh::CoordType tt = (m.face[f_i].V(0)->N()+m.face[f_i].V(1)->N()+m.face[f_i].V(2)->N())/3;
+	tt=tt/sqrt(tt.dot(tt));
+	dis[i] = minDist;
+	if (signo == 1)
+	  {
+	    Point3f dif = clost - currp;
+	    float sign = dif.dot(tt);	
+	    if (sign < 0)
+	      { 
+		dis[i] = -dis[i] ;
+	      }	
+	  }
+
+	outmesh.vert[i].N() = tt;
+      }
+    //write back output
+      vi=outmesh.vert.begin();
+      for (i=0; i<d; i++) 
+      {
+	clost[i*3] = (*vi).P()[0];
+	clost[i*3+1] = (*vi).P()[1];
+	clost[i*3+2] = (*vi).P()[2];
+	normals[i*3] = (*vi).N()[0];
+	normals[i*3+1] = (*vi).N()[1];
+	normals[i*3+2] = (*vi).N()[2];
+	++vi;
+      }
     //tri::io::ExporterPLY<MyMesh>::Save(m,"tt.ply",tri::io::Mask::IOM_VERTNORMAL, false); // in ASCII
   }
    
+
 }
