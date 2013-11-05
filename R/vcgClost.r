2ab9020680579ecb496ec906d360e75c3bace66f
zarquon42b <zarquon42@gmail.com>
2013-11-05 08:50:15 +0100

    renamed vcgIntersect to vcgRaySearchh and updated docu

--- R/vcgClost.r
+++ R/vcgClost.r
@@ -11,8 +11,8 @@
 #' points are returned.
 #' @param smoothNormals logical: if TRUE, laplacian smoothed normals are used.
 #' @return returns an object of class "mesh3d" with:
-#' \item{vb }{ 4xn matrix containing n vertices as homolougous coordinates.}
-#' \item{normals }{4xn matrix containing vertex normals.}
+#' \item{vb }{ 4 x n matrix containing n vertices as homolougous coordinates.}
+#' \item{normals }{4 x n matrix containing vertex normals.}
 #' \item{quality }{vector: containing distances to target.}
 #' \item{it }{4xm matrix containing vertex indices forming triangular
 #' faces.Only available, when x is a mesh.}
@@ -26,6 +26,10 @@
 #' @references Baerentzen, Jakob Andreas. & Aanaes, H., 2002. Generating Signed
 #' Distance Fields From Triangle Meshes. Informatics and Mathematical
 #' Modelling.
+#'
+#' @examples
+#' data(humface)
+#' clost <- vcgClost(humface.lm, humface)
 #' @keywords ~kwd1 ~kwd2
 #' 
 #' 
@@ -37,6 +41,8 @@ vcgClost <- function(x,mesh,sign=TRUE,barycentric=FALSE, smoothNormals=FALSE)
         if (!inherits(mesh,"mesh3d"))
             stop("argument 'mesh' needs to be object of class 'mesh3d'")
         vb <- mesh$vb[1:3,]
+        if (is.null(mesh$it))
+            stop("mesh needs at least some faces")
         it <- mesh$it - 1
         dimit <- dim(it)[2]
         dimvb <- dim(vb)[2]
@@ -46,9 +52,12 @@ vcgClost <- function(x,mesh,sign=TRUE,barycentric=FALSE, smoothNormals=FALSE)
             clost <- t(x)
             x <- list()
             x$vb <- clost
-        } else {
+            class(x) <- "mesh3d"
+        } else if (inherits(x,"mesh3d")) {
             clost <- x$vb[1:3,]
-        }
+        } else
+            stop("x must be a matrix or an object of class mesh3d")
+       
         
         border <- rep(0,ncol(x$vb))
         storage.mode(border) <- "integer"
@@ -61,7 +70,7 @@ vcgClost <- function(x,mesh,sign=TRUE,barycentric=FALSE, smoothNormals=FALSE)
         barycentric <- as.integer(barycentric)
         smooth <- as.integer(smoothNormals)
         tmp <- .C("Rclost",vb,ncol(vb),it,ncol(it),clost,clostDim,clost,dis,sign,border,barycentric,barycoord,faceptr=faceptr,smooth)
-        x$vb[1:3,] <- tmp[[5]]
+        x$vb <- rbind(tmp[[5]],1)
         x$normals <- rbind(tmp[[7]],1)
         chcknorm <- which(is.nan(x$normals))
         if (length(chcknorm) > 0)
--- R/vcgRaySearch.r
+++ R/vcgRaySearch.r
@@ -6,13 +6,28 @@
 #' @param tol minimum distance to target mesh
 #' @details project a mesh along a set of given rays (stored as normals) onto a target and return the hit points as well as information if the target mesh was hit at all. If nothing is hit along the ray, the original point's value will be retrurned. If the point is already on the surface distance will be 0 but be considered as non intersecting along its normal.
 #' @return list with following items:
-#' \item{vb }{3 x n matrix containing intersection points}
+#' \item{vb }{4 x n matrix containing intersection points}
 #' \item{normals }{4 x n matrix containing homogenous coordinates of normals at intersection points}
 #' \item{quality }{integer vector containing a value for each vertex of \code{x}: 1 indicates that a ray has intersected 'mesh' , while 0 means not}
 #' \item{distance }{numeric vector: distances to intersection}
-#' 
-#' @export vcgIntersect
-vcgIntersect <- function(x,mesh,tol=0)
+#' @examples
+#' data(humface)
+#' #get normals of landmarks
+#' lms <- vcgClost(humface.lm, humface)
+#' # offset landmarks along their normals for a negative amount of -5mm
+#' lms$vb[1:3,] <- lms$vb[1:3,]+lms$normals[1:3,]*-5
+#' intersect <- vcgRaySearch(lms, humface)
+#' \dontrun{
+#' require(Morpho)
+#' require(rgl)
+#' spheres3d(vert2points(lms),radius=0.5,col=3)
+#' plotNormals(lms,long=5)
+#' spheres3d(vert2points(intersect),col=2) #plot intersections
+#' wire3d(humface,col="white")#'
+#' }
+#'
+#' @export vcgRaySearch
+vcgRaySearch <- function(x,mesh,tol=0)
 {
   if (!inherits(mesh,"mesh3d") || !inherits(x,"mesh3d"))
             stop("arguments 'x' and 'mesh' needs to be object of class 'mesh3d'")
@@ -29,9 +44,8 @@ vcgIntersect <- function(x,mesh,tol=0)
   dis <- rep(0,clostDim)
   hit <- dis
   storage.mode(hit) <- "integer"
-  tmp <- .Call("R
-intersect",vb,it,clost,normals,tol)
-  x$vb[1:3,] <- tmp$vb
+  tmp <- .Call("Rintersect",vb,it,clost,normals,tol)
+  x$vb <- rbind(tmp$vb,1)
   x$normals <- rbind(tmp$normals,1)
   x$quality <- tmp$hitbool
   x$distance <- tmp$dis
