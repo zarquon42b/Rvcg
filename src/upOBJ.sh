d1e95cefac5a70c50437ad1f9fa8c8fe258df6e4
zarquon42b <zarquon42@gmail.com>
2014-01-16 14:18:56 +0100

    removed gcc extension from Makevars* and added script to updated OBJECTS line

---
+++ src/upOBJ.sh
@@ -0,0 +1,9 @@
+#! /bin/bash
+srces=$(find . -maxdepth 1 -name "*.cpp")
+srces=$(echo $srces | sed "s|./||g")
+srces=$(echo $srces | sed "s|.cpp|.o|g")
+srces="OBJECTS=vcglib/wrap/ply/plylib.o $srces"
+echo $srces 
+sed -i '/OBJECTS=/c\'"$srces"'' Makevars 
+#sed -i 's|OBJECTS|'"$srces"'|g' Makevars
+sed -i '/OBJECTS=/c\'"$srces"'' Makevars.in.win
