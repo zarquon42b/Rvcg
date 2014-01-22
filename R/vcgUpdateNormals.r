624608660ff2a0b0b964c241c30274357d7d8213
zarquon42b <zarquon42@gmail.com>
2014-01-22 13:26:02 +0100

    added option to calculate normals for pointclouds

--- R/vcgUpdateNormals.r
+++ R/vcgUpdateNormals.r
@@ -1,8 +1,9 @@
-#' update vertex normals of a triangular mesh
+#' update vertex normals of a triangular meshes or point clouds
 #'
-#' update vertex normals of a triangular mesh
+#' update vertex normals of a triangular meshes or point clouds
 #' @param mesh triangular mesh of class 'mesh3d'
 #' @param type select the method to compute per-vertex normals: 0=area weighted average of surrounding face normals; 1 = angle weighted vertex normals.
+#' @param pointcloud add options for normal calculation of point clouds.
 #'
 #' @return mesh with updated/created normals
 #'
@@ -12,13 +13,19 @@
 #' humface <- vcgUpdateNormals(humface)
 #' @export
 
-vcgUpdateNormals <- function(mesh,type = 0)
+vcgUpdateNormals <- function(mesh,type = 0, pointcloud=c(10,0))
     {
+        if (is.matrix(mesh)) {
+            tmp <- list()
+            tmp$vb <- t(mesh)
+        }
         vb <- mesh$vb[1:3,]
         it <- mesh$it-1
+        if (! prod(is.integer(pointcloud)))
+            pointcloud <- c(10,0)
         if (! type %in% c(0,1))
             stop("please set valid type")
-        normals <- .Call("RupdateNormals", vb, it, type)
+        normals <- .Call("RupdateNormals", vb, it, type, pointcloud)
         mesh$normals <- rbind(normals,1)
         return(mesh)
     }
--- src/RupdateNormals.cpp
+++ src/RupdateNormals.cpp
@@ -1,6 +1,7 @@
 #include "typedef.h"
 #include "RvcgIO.h"
 #include <Rcpp.h>
+#include <vcg/complex/algorithms/pointcloud_normal.h>
 
 //using namespace vcg;
 using namespace tri;
@@ -8,34 +9,42 @@ using namespace Rcpp;
 //using namespace std;
 
 
-RcppExport SEXP RupdateNormals(SEXP _vb, SEXP _it, SEXP _type)
+RcppExport SEXP RupdateNormals(SEXP _vb, SEXP _it, SEXP _type, SEXP pointcloud_)
 {
   // declare Mesh and helper variables
   int select = Rcpp::as<int>(_type);  
+  Rcpp::IntegerVector pointcloud(pointcloud_);
   MyMesh m;
   VertexIterator vi;
   FaceIterator fi;
   // allocate mesh and fill it
-  Rvcg::IOMesh<MyMesh>::RvcgReadR(m,_vb,_it);
+  int check = Rvcg::IOMesh<MyMesh>::RvcgReadR(m,_vb,_it);
+   Rcpp::NumericMatrix normals(3,m.vn);
   /*m.vert.EnableVFAdjacency();
-  m.face.EnableFFAdjacency();
-  m.face.EnableVFAdjacency();*/
-  
-   
-  // update normals
-  if (select == 0) {
-    tri::UpdateNormal<MyMesh>::PerVertex(m);
-  } else {
-    tri::UpdateNormal<MyMesh>::PerVertexAngleWeighted(m);
-  }
-    tri::UpdateNormal<MyMesh>::NormalizePerVertex(m);
-  Rcpp::NumericMatrix normals(3,m.vn);
-    
-  //write back
-  
+    m.face.EnableFFAdjacency();
+    m.face.EnableVFAdjacency();*/
+   if (check == 1) {
+     Rprintf("%s\n","Info: mesh has no faces normals for point clouds are computed");
+     PointCloudNormal<MyMesh>::Param p;
+     p.fittingAdjNum = pointcloud(0);
+     p.smoothingIterNum = pointcloud(1);
+     p.viewPoint = Point3f(0,0,0);
+     p.useViewPoint = false;
+     PointCloudNormal<MyMesh>::Compute(m,p);
+   }  else {
+     // update normals
+     if (select == 0) {
+       tri::UpdateNormal<MyMesh>::PerVertex(m);
+     } else {
+       tri::UpdateNormal<MyMesh>::PerVertexAngleWeighted(m);
+     }
+     tri::UpdateNormal<MyMesh>::NormalizePerVertex(m);
+     
+     //write back
+     
+   }
   vi=m.vert.begin();
-  SimpleTempData<MyMesh::VertContainer,int> indiceout(m.vert);
- 
+    SimpleTempData<MyMesh::VertContainer,int> indiceout(m.vert);
   for (int i=0;  i < m.vn; i++) {
     if( ! vi->IsD() )	{
       normals(0,i) = (*vi).N()[0];
@@ -46,6 +55,7 @@ RcppExport SEXP RupdateNormals(SEXP _vb, SEXP _it, SEXP _type)
   }
     
   return Rcpp::wrap(normals);
+  
 }
  
 
