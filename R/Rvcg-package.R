8dda9e41b48d82d558f9a8c3865a697557fcfa46
zarquon42b <zarquon42@gmail.com>
2014-01-29 10:12:48 +0100

    pushed version to 0.7.1

--- R/Rvcg-package.R
+++ R/Rvcg-package.R
@@ -6,8 +6,8 @@
 #' \tabular{ll}{
 #' Package: \tab Rvcg\cr
 #' Type: \tab Package\cr
-#' Version: \tab 0.7.140128\cr
-#' Date: \tab 2014-01-28\cr
+#' Version: \tab 0.7.1\cr
+#' Date: \tab 2014-01-29\cr
 #' License: \tab GPL\cr
 #' LazyLoad: \tab yes\cr }
 #' 
