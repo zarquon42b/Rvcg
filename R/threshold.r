647ef19f2d5de4ce03ef854aa86e2b9ee9b8c12f
zarquon42b <zarquon42@gmail.com>
2014-05-19 14:09:23 +0200

    removed obsolete threshold function

--- R/threshold.r
+++
@@ -1,6 +0,0 @@
-threshold <- function(array,lowerbound=0,upperbound=0) {
-    storage.mode(array) <- "integer"
-    nlen <- as.integer(length(array))
-    out <- .Call("threshold",array,lowerbound,upperbound)
-    return(out)
-}
