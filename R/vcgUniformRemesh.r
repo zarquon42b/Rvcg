4153ccc0e5cb5829d9ffcb59e7d84db3f4726d8f
zarquon42b <zarquon42@gmail.com>
2014-11-19 17:35:12 +0100

    added example and docu to vcgUniformRemesh

--- R/vcgUniformRemesh.r
+++ R/vcgUniformRemesh.r
@@ -4,14 +4,18 @@
 #' 
 #' @param x triangular mesh
 #' @param voxelSize voxel size for space discretization
-#' @param offset see meshlab
-#' @param discretize see meshlab
-#' @param multiSample see meshlab
-#' @param absDist see meshlab
+#' @param offset Offset of the created surface (i.e. distance of the created surface from the original one).
+#' @param discretize If TRUE, the position of the intersected edge of the marching cube grid is not computed by linear interpolation, but it is placed in fixed middle position. As a consequence the resampled object will look severely aliased by a stairstep appearance.
+#' @param multiSample If TRUE, the distance field is more accurately compute by multisampling the volume (7 sample for each voxel). Much slower but less artifacts.
+#' @param absDist If TRUE, an unsigned distance field is computed. In this case you have to choose a not zero Offset and a double surface is built around the original surface, inside and outside.
 #' @param mergeClost logical: merge close vertices
 #' @param silent logical: suppress messages
 #' @return resampled mesh
-#'
+#' \dontrun{
+#' data(humface)
+#' humresample <- vcgUniformRemesh(humface,voxelSize=1,multiSample = TRUE)
+#' shade3d(humresample,col=3)
+#' }
 #' @export
 vcgUniformRemesh <- function(x,voxelSize=NULL,offset=0, discretize=FALSE, multiSample=FALSE,absDist=FALSE, mergeClost=FALSE,silent=FALSE) {
     if (is.null(voxelSize))
