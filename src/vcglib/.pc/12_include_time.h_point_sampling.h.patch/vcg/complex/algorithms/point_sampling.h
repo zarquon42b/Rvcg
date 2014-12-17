ef8059f7c8a33a9c9655c0c6fb846058cc9f0947
zarquon42b <zarquon42@gmail.com>
2014-12-17 13:24:05 +0100

    reran patching

--- src/vcglib/.pc/12_include_time.h_point_sampling.h.patch/vcg/complex/algorithms/point_sampling.h
+++ src/vcglib/.pc/12_include_time.h_point_sampling.h.patch/vcg/complex/algorithms/point_sampling.h
@@ -47,7 +47,6 @@ sampling strategies (montecarlo, stratified etc).
 #include <vcg/complex/algorithms/update/bounding.h>
 #include <vcg/complex/algorithms/update/flag.h>
 #include <vcg/space/segment2.h>
-#include <time.h>
 
 namespace vcg
 {
