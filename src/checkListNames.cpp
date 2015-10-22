d19796f6a099a5cffc6866679bd2585951e633b1
zarquon42b <zarquon42@gmail.com>
2015-10-22 09:40:25 +0200

    fixed checkListNames

---
+++ src/checkListNames.cpp
@@ -0,0 +1,16 @@
+#include <checkListNames.h>
+
+std::vector<bool> checkListNames(List mylist, Rcpp::CharacterVector mychar) {
+  try {
+    
+    Rcpp::CharacterVector nam = mylist.names();
+    Rcpp::IntegerVector ind(Rf_match(nam,mychar,0));
+    Rcpp::LogicalVector   log(ind);
+    std::vector<bool> out = Rcpp::as<std::vector<bool>>(log);
+    return out;
+  } catch (std::exception& e) {
+    ::Rf_error( e.what());
+  } catch (...) {
+    ::Rf_error("unknown exception");
+  }
+}
--- src/checkListNames.h
+++ src/checkListNames.h
@@ -4,18 +4,5 @@
 #include <Rcpp.h>
 using Rcpp::List;
 
-std::vector<bool> checkListNames(List mylist, Rcpp::CharacterVector mychar) {
-  try {
-    
-    Rcpp::CharacterVector nam = mylist.names();
-    Rcpp::IntegerVector ind(Rf_match(nam,mychar,0));
-    Rcpp::LogicalVector   log(ind);
-    std::vector<bool> out = Rcpp::as<std::vector<bool>>(log);
-    return out;
-  } catch (std::exception& e) {
-    ::Rf_error( e.what());
-  } catch (...) {
-    ::Rf_error("unknown exception");
-  }
-}
+std::vector<bool> checkListNames(List mylist, Rcpp::CharacterVector mychar);
 #endif
