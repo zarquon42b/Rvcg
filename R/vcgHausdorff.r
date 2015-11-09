187b47efffe4a158ca0ae21e81428fe1326a2b40
Celli119 <francois.girinon@gadz.org>
2015-11-09 17:44:16 +0100

    some cleaning

--- R/vcgHausdorff.r
+++ R/vcgHausdorff.r
@@ -22,35 +22,12 @@
 #' meshDist(humface,distvec=curv$meanvb,from=-0.2,to=0.2,tol=0.01)
 #' }
 #' @export vcgHausdorff
-vcgHausdorff <- function(ref, target)
-	{
-		if (!inherits(ref,"mesh3d"))
-			stop("argument 'ref' needs to be object of class 'mesh3d'")
-		if (!inherits(target,"mesh3d"))
-			stop("argument 'target' needs to be object of class 'mesh3d'")
-		meshes <- list(ref,target)
-		vb <- list()
-		it <- list()
-		for (i in 1:length(meshes)) {
-			meshes[[i]] <- meshintegrity(meshes[[i]],facecheck=TRUE)
-			vb[[i]] <- meshes[[i]]$vb[1:3,,drop=FALSE]
-			it[[i]] <- meshes[[i]]$it - 1
-			storage.mode(it[[i]]) <- "integer"
-		}
+vcgHausdorff <- function(ref, target) {
+   if (!inherits(ref,"mesh3d"))
+       stop("argument 'ref' needs to be object of class 'mesh3d'")
+   if (!inherits(target,"mesh3d"))
+       stop("argument 'target' needs to be object of class 'mesh3d'")
 
-		# ref <- meshintegrity(ref,facecheck=TRUE)
-		# target <- meshintegrity(target,facecheck=TRUE)
-		# vb0 <- ref$vb0[1:3,,drop=FALSE]
-		# it0 <- ref$it0 - 1
-		# dimit0 <- dim(it0)[2]
-		# dimvb0 <- dim(vb0)[2]
-		# storage.mode(it0) <- "integer"
-
-		# vb1 <- target$vb1[1:3,,drop=FALSE]
-		# it1 <- target$it1 - 1
-		# dimit1 <- dim(it1)[2]
-		# dimvb1 <- dim(vb1)[2]
-		# storage.mode(it) <- "integer"
-		tmp <- .Call("Rhausdorff",vb[1],it[1],vb[2],it[2])
-		return(tmp)
-	}
+   tmp <- .Call("Rhausdorff",ref$vb[1:3,],ref$it-1L,target$vb[1:3,],target$it-1L)
+   return(tmp)
+}
