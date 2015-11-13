df377035912934cbf8a5726d28115aed4e45a02a
Celli119 <francois.girinon@gadz.org>
2015-11-13 18:12:18 +0100

    change typedef names to avoid conflicts with vcgQEdecim

--- src/typedefHausdorff.h
+++ src/typedefHausdorff.h
@@ -36,11 +36,11 @@ using namespace std;
 #define PRINT_EVERY_N_ELEMENTS          1000
 
 
-class CFace;
-class CVertex;
-struct UsedTypes:public vcg::UsedTypes< vcg::Use<CFace>::AsFaceType, vcg::Use<CVertex>::AsVertexType>{};
-class CVertex   : public vcg::Vertex<UsedTypes,vcg::vertex::Coord3d,vcg::vertex::Qualityf,vcg::vertex::Normal3d,vcg::vertex::Color4b,vcg::vertex::BitFlags> {};
-class CFace     : public vcg::Face< UsedTypes,vcg::face::VertexRef, vcg::face::Normal3d, vcg::face::EdgePlane,vcg::face::Color4b,vcg::face::Mark,vcg::face::BitFlags> {};
-class CMesh     : public vcg::tri::TriMesh< std::vector<CVertex>, std::vector<CFace> > {};
+class CFaceH;
+class CVertexH;
+struct UsedTypes:public vcg::UsedTypes< vcg::Use<CFaceH>::AsFaceType, vcg::Use<CVertexH>::AsVertexType>{};
+class CVertexH   : public vcg::Vertex<UsedTypes,vcg::vertex::Coord3d,vcg::vertex::Qualityf,vcg::vertex::Normal3d,vcg::vertex::Color4b,vcg::vertex::BitFlags> {};
+class CFaceH     : public vcg::Face< UsedTypes,vcg::face::VertexRef, vcg::face::Normal3d, vcg::face::EdgePlane,vcg::face::Color4b,vcg::face::Mark,vcg::face::BitFlags> {};
+class CMesh     : public vcg::tri::TriMesh< std::vector<CVertexH>, std::vector<CFaceH> > {};
 
 #endif
