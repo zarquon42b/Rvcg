5dec01f60646b60d9a142fc0707b78322420520d
zarquon42b <zarquon42@gmail.com>
2015-12-15 09:07:57 +0100

    fixed template instantiation causing win to fail building

--- src/checkListNames.cpp
+++ src/checkListNames.cpp
@@ -6,7 +6,7 @@ std::vector<bool> checkListNames(List mylist, Rcpp::CharacterVector mychar) {
     Rcpp::CharacterVector nam = mylist.names();
     Rcpp::IntegerVector ind(Rf_match(nam,mychar,0));
     Rcpp::LogicalVector   log(ind);
-    std::vector<bool> out = Rcpp::as<std::vector<bool>>(log);
+    std::vector<bool> out = Rcpp::as<std::vector<bool> >(log);
     return out;
   } catch (std::exception& e) {
     ::Rf_error( e.what());
