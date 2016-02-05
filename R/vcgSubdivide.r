71cfb35757f6b5396b65bdb77a96223b130f3f4b
zarquon42b <zarquon42@gmail.com>
2016-02-05 16:28:48 +0100

    added vcgSubdivide

---
+++ R/vcgSubdivide.r
@@ -0,0 +1,29 @@
+#' subdivide the triangles of a mesh
+#'
+#' subdivide the triangles of a mesh
+#' @param x triangular mesh of class "mesh3d"
+#' @param threshold minimum edge length to subdivide
+#' @param type character: algorithm used. Options are Butterfly and Loop (see notes)
+#' @param looptype character: method for type = loop options are "loop","regularity","continuity" (see notes)
+#' @param iterations integer: number of iterations
+#' @param silence logical: suppress output.
+#' @return returns subdivided mesh
+#' @note
+#' The different algorithms are (from meshlab description):
+#' \itemize{
+#' \item{\bold{Butterfly Subdivision:} Apply Butterfly Subdivision Surface algorithm. It is an interpolated method, defined on arbitrary triangular meshes. The scheme is known to be C1 but not C2 on regular meshes}
+#' \item{\bold{Loop Subdivision:} Apply Loop's Subdivision Surface algorithm. It is an approximant subdivision method and it works for every triangle and has rules for extraordinary vertices. Options are "loop" a simple subdivision, "regularity" to enhance the meshe's regularity and "continuity" to enhance the mesh's continuity.}
+#' }
+#' @export
+vcgSubdivide <- function(x, threshold=NULL, type=c("Butterfly","Loop"),looptype=c("loop","regularity","continuity"),iterations=3, silent= FALSE) {
+    typeargs <- c("butterfly","loop")
+    if (is.null(threshold))
+        threshold <- -1
+    type <- match.arg(tolower(type[1]),typeargs)
+    type <- match(type,typeargs)-1
+    loopargs <- c("loop","regularity","continuity")
+    looptype <- match.arg(tolower(looptype[1]),loopargs)
+    looptype <- match(looptype,loopargs)-1
+    out <- .Call("Rsubdivision",x$vb[1:3,], x$it-1L,iterations,threshold,type,looptype,silent)
+    return(out)
+}
---
+++ src/Rsubdivision.cpp
@@ -0,0 +1,79 @@
+// Author: Stefan Schlager
+// This is based on code from 
+// of trimeshinfo
+// included in the vcglib sources
+// to work with R
+#include "typedefTopo.h"
+#include <vcg/complex/algorithms/refine_loop.h>
+
+#include "RvcgIO.h"
+#include <Rcpp.h>
+
+using namespace vcg;
+using namespace tri;
+using namespace Rcpp;
+
+RcppExport SEXP Rsubdivision(SEXP vb_ , SEXP it_,SEXP iterations_, SEXP threshold_,SEXP type_,SEXP looptype_, SEXP silent_) {
+  try { 
+    // declare Mesh and helper variables
+    int i;
+    TopoMyMesh m;
+    VertexIterator vi;
+    FaceIterator fi;
+    float threshold = as<float>(threshold_);
+    int iterations = as<int>(iterations_);
+    int check = Rvcg::IOMesh<TopoMyMesh>::RvcgReadR(m,vb_,it_);
+    int type = as<int>(type_);
+    int looptype = as<int>(looptype_);
+    bool silent = as<bool>(silent_);
+    /*m.vert.EnableVFAdjacency();
+      m.face.EnableFFAdjacency();
+      m.face.EnableVFAdjacency();*/
+    if (check != 0) {
+      ::Rf_error("mesh has no faces and/or no vertices, nothing done");
+    }  else {
+      
+      tri::UpdateFlags<TopoMyMesh>::FaceBorderFromFF(m);
+      tri::UpdateTopology<TopoMyMesh>::FaceFace(m);
+      if (tri::Clean<TopoMyMesh>::CountNonManifoldEdgeFF(m) > 0)
+	::Rf_error("Mesh has some not 2 manifoldfaces, subdivision surfaces require manifoldness");
+      if (threshold < 0) {
+	tri::UpdateBounding<TopoMyMesh>::Box(m);
+	threshold = m.bbox.Diag()*0.01;
+	if (!silent)
+	  Rprintf("Edge Threshold set to %f\n",threshold);
+      }
+      for(int i=0; i< iterations; ++i) {
+	if (type == 0) {
+	  Refine<TopoMyMesh,MidPointButterfly<TopoMyMesh> >(m,MidPointButterfly<TopoMyMesh>(m),threshold);
+	} else if (type == 1) {
+	  switch(looptype) {
+	  case 0:
+	    tri::RefineOddEven<TopoMyMesh>(m, tri::OddPointLoop<TopoMyMesh>(m), tri::EvenPointLoop<TopoMyMesh>(), threshold);
+	    break;
+	  case 1:
+	    tri::RefineOddEven<TopoMyMesh>(m, tri::OddPointLoopGeneric<TopoMyMesh, Centroid<TopoMyMesh>, RegularLoopWeight<TopoMyMesh::ScalarType> >(m),
+					   tri::EvenPointLoopGeneric<TopoMyMesh, Centroid<TopoMyMesh>, RegularLoopWeight<TopoMyMesh::ScalarType> >(), threshold);
+	    break;
+	  case 2:
+	    tri::RefineOddEven<TopoMyMesh>(m, tri::OddPointLoopGeneric<TopoMyMesh, Centroid<TopoMyMesh>, ContinuityLoopWeight<TopoMyMesh::ScalarType> >(m),
+					   tri::EvenPointLoopGeneric<TopoMyMesh, Centroid<TopoMyMesh>, ContinuityLoopWeight<TopoMyMesh::ScalarType> >(), threshold);
+	    break;
+        
+	  }
+	}
+      }
+    }
+  
+    return Rvcg::IOMesh<TopoMyMesh>::RvcgToR(m);
+  } catch (std::exception& e) {
+    ::Rf_error( e.what());
+  } catch (...) {
+    ::Rf_error("unknown exception");
+  }
+}
+
+
+
+
+
