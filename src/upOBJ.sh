760d9b559072ff136e9e511d3c67bd1ec21a2180
Stefan Schlager <zarquon42@gmail.com>
2016-03-25 18:18:51 +0100

    fixed upOBJ.sh

--- src/upOBJ.sh
+++ src/upOBJ.sh
@@ -4,6 +4,6 @@ srces=$(echo $srces | sed "s|./||g")
 srces=$(echo $srces | sed "s|.cpp|.o|g")
 srces="OBJECTS=vcglib/wrap/ply/plylib.o $srces"
 echo $srces 
-sed -i '/OBJECTS=/c\'"$srces"'' Makevars.in 
+sed -i '/OBJECTS=/c\'"$srces"'' Makevars 
 #sed -i 's|OBJECTS|'"$srces"'|g' Makevars
 #sed -i '/OBJECTS=/c\'"$srces"'' Makevars.win
