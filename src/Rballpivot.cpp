5915cd12a3dc54cf406e7ef7455d6b5081393b34
Stefan Schlager <zarquon42@gmail.com>
2016-03-25 20:00:05 +0100

    some minor code formatting

--- src/Rballpivot.cpp
+++ src/Rballpivot.cpp
@@ -13,22 +13,22 @@ using namespace Rcpp;
 
 RcppExport SEXP Rballpivoting(SEXP mesh_, SEXP Radius_, SEXP Clustering_, SEXP CreaseThr_, SEXP deleteFaces_) {
   try {
-  TopoMyMesh mesh;
-  double Radius = as<double>(Radius_);
-  double Clustering = as<double>(Clustering_);
-  double CreaseThr = as<double>(CreaseThr_);
-  bool deleteFaces = as<bool>(deleteFaces_);
-  if (deleteFaces) {
-    mesh.fn = 0;
-    mesh.face.resize(0);
-  }
-  Rvcg::IOMesh<TopoMyMesh>::mesh3d2Rvcg(mesh,mesh_);
-  tri::BallPivoting<TopoMyMesh> pivot(mesh, Radius, Clustering, CreaseThr);           
-  pivot.BuildMesh();
+    TopoMyMesh mesh;
+    double Radius = as<double>(Radius_);
+    double Clustering = as<double>(Clustering_);
+    double CreaseThr = as<double>(CreaseThr_);
+    bool deleteFaces = as<bool>(deleteFaces_);
+    if (deleteFaces) {
+      mesh.fn = 0;
+      mesh.face.resize(0);
+    }
+    Rvcg::IOMesh<TopoMyMesh>::mesh3d2Rvcg(mesh,mesh_);
+    tri::BallPivoting<TopoMyMesh> pivot(mesh, Radius, Clustering, CreaseThr);           
+    pivot.BuildMesh();
  
-return Rvcg::IOMesh<TopoMyMesh>::RvcgToR(mesh);
+    return Rvcg::IOMesh<TopoMyMesh>::RvcgToR(mesh);
  
- } catch (std::exception& e) {
+  } catch (std::exception& e) {
     ::Rf_error( e.what());
   } catch (...) {
     ::Rf_error("unknown exception");
