e84e0a701a305fbfa8df9df9a2491a09894193cd
Tim Schäfer <ts+code@rcmd.org>
2021-07-02 11:01:37 +0200

    add visualization examples

--- R/vcgUpdateNormals.r
+++ R/vcgUpdateNormals.r
@@ -46,6 +46,12 @@ vcgUpdateNormals <- function(mesh,type = 0, pointcloud=c(10,0), silent=FALSE)
     }
 
 
+### Show vertex normals:
+# hf = Rvcg::vcgUpdateNormals(humface)
+# vis.fs.surface(humface)
+# fsbrain::highlight.points.spheres(t(hf$vb[1:3,1:200]), color="green", radius=0.5)
+# fsbrain::highlight.points.spheres(t(hf$normals[1:3,1:200])+t(hf$vb[1:3,1:200]), color="red", radius=0.5)
+
 #' @title Compute face normals for a mesh.
 #'
 #' @param mesh triangular mesh of class 'mesh3d', from \code{rgl}
@@ -64,3 +70,9 @@ vcgFaceNormals <- function(mesh) {
     return(out);
 }
 
+### Show face normals:
+# bary = vcgBary(humface);
+# fn = Rvcg::vcgFaceNormals(humface);
+# vis.fs.surface(humface);
+# fsbrain::highlight.points.spheres(bary[1:200,], color="green", radius=0.5)
+# fsbrain::highlight.points.spheres(bary[1:200,] + t(fn)[1:200,], color="red", radius=0.5)
