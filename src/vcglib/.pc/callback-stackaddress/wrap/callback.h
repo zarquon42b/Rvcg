950546bedc46f8ff0261a4c7c0c789c65c229bf5
Stefan Schlager <zarquon42@gmail.com>
2026-02-16 12:47:38 +0100

    CRAN Release

--- src/vcglib/.pc/callback-stackaddress/wrap/callback.h
+++ src/vcglib/.pc/callback-stackaddress/wrap/callback.h
@@ -60,23 +60,23 @@ inline bool CErrCallBackPos(const int ,  const char *str ) {
  * Quite useful for formatting the above callback... 
  */
 
-// inline char* StrFormat(const std::string fmt_str, ...) {
-//     int final_n, n = ((int)fmt_str.size()) * 2; /* Reserve two times as much as the length of the fmt_str */
-//     std::unique_ptr<char[]> formatted;
-//     va_list ap;
-//     while(1) {
-//         formatted.reset(new char[n]); /* Wrap the plain char array into the unique_ptr */
-//         strcpy(&formatted[0], fmt_str.c_str());
-//         va_start(ap, fmt_str);
-//         final_n = vsnprintf(&formatted[0], n, fmt_str.c_str(), ap);
-//         va_end(ap);
-//         if (final_n < 0 || final_n >= n)
-//             n += abs(final_n - n + 1);
-//         else
-//             break;
-//     }
-//     return formatted.get();
-// }
+inline char* StrFormat(const std::string fmt_str, ...) {
+    int final_n, n = ((int)fmt_str.size()) * 2; /* Reserve two times as much as the length of the fmt_str */
+    std::unique_ptr<char[]> formatted;
+    va_list ap;
+    while(1) {
+        formatted.reset(new char[n]); /* Wrap the plain char array into the unique_ptr */
+        strcpy(&formatted[0], fmt_str.c_str());
+        va_start(ap, fmt_str);
+        final_n = vsnprintf(&formatted[0], n, fmt_str.c_str(), ap);
+        va_end(ap);
+        if (final_n < 0 || final_n >= n)
+            n += abs(final_n - n + 1);
+        else
+            break;
+    }
+    return formatted.get();
+}
 
 }// End namespace
 
